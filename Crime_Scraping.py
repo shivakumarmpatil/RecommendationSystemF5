9db20b8567e51306db88c0c1b8cf9a8a8988cfa9
musab7890 <57774983+musab7890@users.noreply.github.com>
2019-11-15 14:58:28 -0500

    Add files via upload

--- Crime_Scraping.py
+++ Crime_Scraping.py
@@ -7,31 +7,38 @@ Created on Fri Nov 15 10:22:03 2019
 import requests
 import pandas as pd
 from bs4 import BeautifulSoup
+headings = []
+full_df =pd.DataFrame() #pd.DataFrame({'City','Theft', 'Robbery', 'Burglary', 'Vandalism', 'Shooting', 'Arson', 'Arrest', 'Assault', 'Other'})
+ind = 0
+city_bor = ['bronx','manhattan','queens','brooklyn','statenisland']
 
-
-try:
-    page = requests.get('https://spotcrime.com/ny/brooklyn')
-    soup = BeautifulSoup(page.text, 'html.parser')
-    table = soup.find("table",attrs ={"class":"table-crime-data"})
-    headings = []
-    
-    headings = [th.get_text() for th in table.find("tr").find_all("th")]
-    
-except:
-    print("Page retrieval issue")  
-data=[]
-datasets = []
-counter =1
-heads = []
-for row in table.findAll("tr"):
-    heads.append(row.find("th").getText())
+for ind in range(5):
     try:
-        for s in row.findAll("td"):
-            if counter == 4:
-                print(heads)print(s.getText())
-                counter = 1
-            else:
-                counter +=1
+        page = requests.get('https://spotcrime.com/ny/'+city_bor[ind])
+        soup = BeautifulSoup(page.text, 'html.parser')
+        table = soup.find("table",attrs ={"class":"table-crime-data"})
     except:
-        print("err")
-            
+        print("Page retrieval issue")  
+    data=[]
+    datasets = []
+    counter =1
+    heads = []
+    c_values = []
+    for row in table.findAll("tr"):
+        heads.append(row.find("th").getText())
+        try:
+            for s in row.findAll("td"):
+                if counter == 4:
+                    c_values.append(s.getText())
+                    counter = 1
+                else:
+                    counter +=1
+        except:
+            print("err")
+    df = pd.DataFrame({'City':[city_bor[ind]],str(heads[1]):[c_values[0]],str(heads[2]):[c_values[1]],str(heads[3]):[c_values[2]],str(heads[4]):[c_values[3]],str(heads[5]):[c_values[4]],str(heads[6]):[c_values[5]],str(heads[7]):[c_values[6]],str(heads[8]):[c_values[7]],str(heads[9]):[c_values[8]]})
+    full_df = full_df.append(df,ignore_index = True)
+    
+full_df.to_csv('Crime_Stats.csv', index=False)
+
+
+#print(c_values)
